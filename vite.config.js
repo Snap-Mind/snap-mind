c58decd0e26796328f882fb81442f06480ee6feb
Louis-7 <louisgh.cn@gmail.com>
2025-09-15 19:35:51 +0800

    chore: merge vite config

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite';
-
-// https://vite.dev/config/
-export default defineConfig({
-  base: './', // Ensure relative asset paths for Electron production
-  plugins: [react(), tailwindcss()],
-});
