bd36f2f57701e319c3d2170c2200e82e4afa4d06
Louis-7 <louisgh.cn@gmail.com>
2026-08-12 08:50:13 +0800

    refactor: hydrate zustand stores at boot alongside ServiceProvider

--- src/main.tsx
+++ src/main.tsx
@@ -3,18 +3,27 @@ import { createRoot } from 'react-dom/client';
 import { HashRouter } from 'react-router';
 
 import { ServiceProvider } from './contexts/ServiceProvider.jsx';
+import { useSettingsStore } from './stores/useSettingsStore';
+import { useChatStore } from './stores/useChatStore';
 
 import './i18n/i18n';
 import './index.css';
 
 import App from './App.jsx';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <ServiceProvider>
-      <HashRouter>
-        <App></App>
-      </HashRouter>
-    </ServiceProvider>
-  </StrictMode>
-);
+async function boot() {
+  await useSettingsStore.getState().hydrate();
+  useChatStore.getState().hydrateFromSettings();
+
+  createRoot(document.getElementById('root')!).render(
+    <StrictMode>
+      <ServiceProvider>
+        <HashRouter>
+          <App />
+        </HashRouter>
+      </ServiceProvider>
+    </StrictMode>
+  );
+}
+
+void boot();
