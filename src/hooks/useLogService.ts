fecac093f3cdab876023c4571de67aec57c9f1a9
Louis-7 <louisgh.cn@gmail.com>
2026-08-12 08:52:51 +0800

    refactor: turn logger + autoupdate into module singletons

--- src/hooks/useLogService.ts
+++ src/hooks/useLogService.ts
@@ -1,11 +1,3 @@
-import { useContext } from 'react';
-import { LoggerServiceContext } from '../contexts/ServiceProvider';
-import { LoggerService } from '../services/LoggerService';
+import loggerService, { LoggerService } from '../services/LoggerService';
 
-export const useLogService = (): LoggerService => {
-  const context = useContext(LoggerServiceContext);
-  if (context === null) {
-    throw new Error('useLogService must be used within a LoggerServiceProvider');
-  }
-  return context;
-};
+export const useLogService = (): LoggerService => loggerService;
